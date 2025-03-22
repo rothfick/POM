42cd196af0aca58cefd7715d0313cd1dc96020c7
rothfick <robertoo256@icloud.com>
2025-03-22 10:59:26 +0100

    Created a DriveFactory for Quality Island POM framework

---
+++ src/main/java/com/pomframewrok/base/DriverFactory.java
@@ -0,0 +1,54 @@
+package com.pomframewrok.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DriverFactory {
+    private static final Logger logger = LoggerFactory.getLogger(DriverFactory.class);
+
+    public static WebDriver getDriver(String browserType) {
+        WebDriver driver = null;
+        
+        switch(browserType.toLowerCase()) {
+            case "chrome":
+                logger.info("Initializing Chrome Driver");
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+                configureCommonOptions(options);
+                driver = new ChromeDriver(options);
+                break; 
+            case "firefox":
+                logger.info("Initializing Firefox Driver");
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                logger.info("Initializing Edge Driver");
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                configureCommonOptions(edgeOptions);
+                driver = new EdgeDriver(edgeOptions);
+                break;
+        }
+        return driver;
+    }
+
+    private static void configureCommonOptions(Object options) {
+        if (options instanceof ChromeOptions) {
+            ChromeOptions chromeOptions = (ChromeOptions) options;
+            chromeOptions.addArguments("--disable-notifications");
+            chromeOptions.addArguments("--start-maximized");
+        } else if (options instanceof  EdgeOptions ) {
+            EdgeOptions edgeOptions = (EdgeOptions) options;
+            edgeOptions.addArguments("--disable-notifications");
+            edgeOptions.addArguments("--start-maximized");
+        }
+    }
+}
